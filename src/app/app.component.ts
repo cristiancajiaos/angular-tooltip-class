96e62dbf7133c4ce1a7a6d9e7c07ee0ba28aed45
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-05-09 18:44:24 -0400

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "tooltip-class";
+
+  tooltipClass = "foo";
+}
